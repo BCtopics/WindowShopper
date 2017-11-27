26a0466cedc6b9663a5ba0e63f8218fc1fb1e871
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-11-27 17:54:37 -0500

    Add Custom Currency Symbol With DrawRect

--- Window-Shopper/Window-Shopper/Views/CurrencyTextField.swift
+++ Window-Shopper/Window-Shopper/Views/CurrencyTextField.swift
@@ -20,10 +20,33 @@ class CurrencyTextField: UITextField {
         customizeView()
     }
     
+    override func draw(_ rect: CGRect) {
+        
+        // Align Correctly
+        
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - (size / 2), width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.8030196655, green: 0.8030196655, blue: 0.8030196655, alpha: 0.7982662671) // Light Gray with 80% Opacity
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        // Get Currency Symbol
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25) // White opacity at 25%
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
